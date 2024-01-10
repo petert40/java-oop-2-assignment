7d432657ecc8358349ecd8ef8b232d23070394de
petert40 <petertang224@gmail.com>
2024-01-09 20:46:54 -0500

    implement java as per requirements

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+I
+src/mobilePhone/Main.java,0\2\02481adb2b9ec17aec3d045c10f98a94f2ae9358
+P
+ src/mobilePhone/MobilePhone.java,0\b\0b5ba5382e3f898b07eb684f09d8b815c7ab799e
+;
+qodana.yaml,8\5\85c3a901608a6b9adf48d245fa84828b66304e51
--- src/mobilePhone/MobilePhone.java
+++ src/mobilePhone/MobilePhone.java
@@ -1,49 +1,106 @@
 package mobilePhone;
 
 public class MobilePhone {
+    public static void main(String[] args) {
+        MobilePhone oneplus = new MobilePhone("Oneplus", "9", "xxx-xxx-xxxx", 512);
+        oneplus.makeCall("585-748-8096");
+        oneplus.installApp("Google Chrome");
+        oneplus.displayInfo();
+    }
 	// Please create 4 instance variables for our MobilePhone class:
 	// 1.Brand
 	// 2.Model
 	// 3.Phone Number
 	// 4.Storage Capacity
 	// Think about what data types would work best with these fields :)
+    private String brand;
+    private String model;
+    private String phoneNumber;
+    private Integer capacity;
+
+    // Let's create a public constructor for our MobilePhone class
+    // Given 4 pieces of information as input (brand, model, operatingSystem, and
+    // storageCapacity),
+    // Set the instance variables of your object accordingly
+    public MobilePhone(String brand, String model, String phoneNumber, Integer capacity){
+        this.brand = brand;
+        this.model = model;
+        this.phoneNumber = phoneNumber;
+        this.capacity = capacity;
+    }
+
+    // 1. A makeCall method that takes a phone number as a parameter and then
+    // prints:
+    // 'We are calling {the input phone number} from our {brand} {model} phone!'
+    public void makeCall(String phoneNumber){
+        System.out.printf("We are calling %s from our %s %s phone!\n", phoneNumber, brand, model);
+    }
+
+    // 2. An installApp method that takes an app name String as input and then
+    // prints:
+    // 'Installing {input app name} on our {brand} {model} phone!'
+    public void installApp(String appName){
+        System.out.printf("Installing %s on our %s %s phone!\n", appName, this.brand, this.model);
+    }
+
+    // 3. Lastly, a displayInfo method that takes no parameters and prints all
+    // information about
+    // the phone:
+    // Brand: {brand}
+    // Model: {model}
+    // Phone Number: {phone number}
+    // Storage Capacity: {storage capacity}GB
+    public void displayInfo(){
+        System.out.println("The phone:");
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Storage Capacity: " + capacity + "GB");
+    }
+
+    // Create getter and setter methods for each of our instance variables
+    // Please at least write 1 set of getter/setter by hand. After you get the
+    // hang of it, you can go to 'source'->'generate getters and setters'->checkmark
+    // all of the fields you want
+    // getters and setters for->'generate'
+    public String getBrand(){
+        return brand;
+    }
+
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
 
 
-	// Let's create a public constructor for our MobilePhone class
-	// Given 4 pieces of information as input (brand, model, operatingSystem, and
-	// storageCapacity),
-	// Set the instance variables of your object accordingly
+
+
 
 	
-	
-	// Create getter and setter methods for each of our instance variables
-	// Please at least write 1 set of getter/setter by hand. After you get the
-	// hang of it, you can go to 'source'->'generate getters and setters'->checkmark
-	// all of the fields you want
-	// getters and setters for->'generate'
 
-
-	// Lets make a couple custom phone-related methods:
-
-	// 1. A makeCall method that takes a phone number as a parameter and then
-	// prints:
-	// 'We are calling {the input phone number} from our {brand} {model} phone!'
-
-
-	
-	// 2. An installApp method that takes an app name String as input and then
-	// prints:
-	// 'Installing {input app name} on our {brand} {model} phone!'
-
-
-	
-	// 3. Lastly, a displayInfo method that takes no parameters and prints all
-	// information about
-	// the phone:
-	// Brand: {brand}
-	// Model: {model}
-	// Phone Number: {phone number}
-	// Storage Capacity: {storage capacity}GB
 
 
 
